51b411ade3774487108e3f7d6db88ee8623c9030
Aswin Asok <aswinasokofficial@gmail.com>
2020-09-30 16:17:06 +0530

    S601 Completed

--- testback/index.js
+++ testback/index.js
@@ -1,13 +1,29 @@
 
 const express = require('express')
-
 const app = express()
-
 const port = 8000;
+const admin = (req,res)=>{
+    return res.send("This is Admindashboard")
+}
 
 app.get("/",(req,res) => {
     return res.send("Home page")
 });
+
+
+
+const isAdmin = (req,res, next)=>{
+    console.log("isAdmin is running");
+    next()
+}
+
+const isLoggedIn = (req,res, next)=>{
+    console.log("isLoggedInis running");
+    next()
+}
+
+
+app.get('/admin',isLoggedIn, isAdmin, admin)
 
 app.get("/signout",(req,res) => {
     return res.send("You are signed out")
