77487d76bca528039908b8fcc691c3f0f6a8ed82
Aswin Asok <aswinasokofficial@gmail.com>
2020-10-02 14:24:47 +0530

    S610 Validation

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,8 +1,13 @@
 var express = require('express')
 var router = express.Router()
+const { check } = require('express-validator');
 const{signout, signup} = require("../controllers/auth")
 
-router.post("/signup", signup)
+router.post("/signup",[
+    check("name", "Name should be at least 3 Characters").isLength({ min: 3 }),
+    check("email", "Email is Required").isEmail(),
+    check("password", "Password shoulf br atleast 3 characters").isLength({min: 3})
+], signup)
 router.get("/signout", signout);
 
 module.exports = router;
