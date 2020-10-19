0b49e4f1e33af7013cbf3539e3ff4b8bc1dcec14
Aswin Asok <aswinasokofficial@gmail.com>
2020-10-19 12:27:33 +0530

    S1008 Update and Delete Routes

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -73,3 +73,58 @@ exports.photo = (req, res, next) => {
   }
   next();
 }
+
+//Delete Controller
+exports.deleteProduct = (req, res) => {
+  let product = req.product;
+  product.remove((err, deletedProduct)=>{
+    if(err){
+      return res.status(400).json({
+        error: "Failed to delete Product"
+      })
+    }
+    res.json({
+      message: "Deletion was success",
+      deletedProduct
+    })
+  })
+}
+
+//Update Controller
+exports.updateProduct = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+
+  form.parse(req, (err, fields, file) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Problem with the Image",
+      });
+    }
+
+    //updation code
+    let product = req.product;
+    product = _.extend(product, fields)
+
+    //Handle file here
+    if (file.photo) {
+      if (file.photo.size > 3000000) {
+        return res.status(404).json({
+          error: "File Size to too big!",
+        });
+      }
+      product.photo.data = fs.readFileSync(file.photo.path);
+      product.photo.contentType = file.photo.type;
+    }
+
+    //Save to the DB
+    product.save((err, product) => {
+      if (err) {
+        return res.status(400).json({
+          error: "Updation of product failed",
+        });
+      }
+      res.json(product);
+    });
+  });
+}
--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -1,19 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
-const {getProductById, createProduct, getProduct,photo} = require("../controllers/product")
+const {getProductById, createProduct, getProduct,photo, deleteProduct, updateProduct} = require("../controllers/product")
 const {isSignedIn, isAuthenticated, isAdmin} = require("../controllers/auth")
 const {getUserById} = require("../controllers/user")
 
-//All of params
+//All of Params
 router.param("userId", getUserById);
 router.param("productId", getProductById)
 
-//all of actual routes
+//All of the actual Routes
+//Create Route
 router.post("/product/create/:userId", isSignedIn,isAuthenticated,isAdmin, createProduct)
 
-
+//Read Routes
 router.get("/product/:productId", getProduct)
 router.get("/product/photo/:productId", photo)
+
+//Delele Route
+router.delete("/product/:productId/:userId", isSignedIn, isAuthenticated, isAdmin, deleteProduct)
+//Update Route
+router.put("/product/:productId/:userId", isSignedIn, isAuthenticated, isAdmin, updateProduct)
+//Listing Route
 
 module.exports = router;
