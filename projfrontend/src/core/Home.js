386774d779455ec130b392f9889f3be321a94e56
Aswin Asok <aswinasokofficial@gmail.com>
2020-10-29 14:46:55 +0530

    S1306 chilldren in the base file as the argument is confusing,  Making the base component

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,14 +1,25 @@
-import React from 'react'
-import "../styles.css"; 
-import {API} from "../backend"
+import React from "react";
+import "../styles.css";
+import { API } from "../backend";
+import Base from "./Base";
 
-function Home(){
-    console.log("API IS : ", API);
-    return(
-        <div>
-            <h1 className = "text-white">Hello FrontEnd</h1>
+function Home() {
+  console.log("API IS : ", API);
+  return (
+    <Base title="Home Page">
+      <div className="row">
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
         </div>
-    )
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
+        </div>
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
+        </div>
+      </div>
+    </Base>
+  );
 }
 
 export default Home;
