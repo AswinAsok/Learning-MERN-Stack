def44a80d29ba2d056ee52139d235300e00e1e57
Aswin Asok <aswinasokofficial@gmail.com>
2020-11-02 08:10:18 +0530

    Conditional rendering for the Nav bar has been completed

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Home from "./core/Home"
+import AdminRoute from "./auth/helper/AdminRoutes";
+import Home from "./core/Home";
 import Signin from "./user/Signin";
 import Signup from "./user/Signup";
-
+import PrivateRoute from "./auth/helper/PrivateRoutes";
+import UserDashboard from "./user/UserDashBoard";
+import AdminDashboard from "./user/AdminDashBoard";
 
 function Routes() {
   return (
@@ -12,6 +15,8 @@ function Routes() {
         <Route path="/" exact component={Home} />
         <Route path="/signup" exact component={Signup} />
         <Route path="/signin" exact component={Signin} />
+        <PrivateRoute path = "/user/dashboard" exact component={UserDashboard}/>
+        <AdminRoute path = "/admin/dashboard" exact component={AdminDashboard}/>
       </Switch>
     </BrowserRouter>
   );
--- projfrontend/src/user/Signin.js
+++ projfrontend/src/user/Signin.js
@@ -49,9 +49,9 @@ const Signin = () => {
 
     if (didRedirect) {
       if (user && user.role === 1) {
-        return <p>Redirect to Admin</p>;
+        return <Redirect to="/admin/dashboard"/>;
       } else {
-        return <p>Redirect to user Dashboard</p>;
+        return <Redirect to="/user/dashboard"/>;
       }
     }
     if (isAuthenticated()) {
