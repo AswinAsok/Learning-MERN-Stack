5272209ffb7ffae68f8c3c83a8fa90e893282e5b
Aswin Asok <aswinasokofficial@gmail.com>
2020-11-07 12:51:22 +0530

    Creating Reuseable Card

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -2,14 +2,14 @@ import React from "react";
 import "../styles.css";
 import { API } from "../backend";
 import Base from "./Base";
+import Card from "./Card";
 
 function Home() {
-  console.log("API IS : ", API);
   return (
     <Base title="Home Page" description="Welcome to the T-Shirt Store">
-      <div className="row">
+      <div className="row text-center">
         <div className="col-4">
-          <button className="btn btn-success">TEST</button>
+          <Card />
         </div>
         <div className="col-4">
           <button className="btn btn-success">TEST</button>
